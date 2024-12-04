f1d9c2dae963f5f76e83ab39b3c28b9a4e9c4a0f
yosof homaid <71443273+yosfgfx@users.noreply.github.com>
2024-12-04 23:58:07 +0300

    temp

--- New/src/components/Logo.tsx
+++ New/src/components/Logo.tsx
@@ -4,7 +4,7 @@ export const Logo: FC = () => {
   return (
     <div className="fixed top-8 right-4 z-20">
       <img 
-        src={`${import.meta.env.BASE_URL}assets/logo-new.png`}
+        src={`${import.meta.env.BASE_URL}logo-new.png`}
         alt="Logo" 
         className="max-w-[260px] w-full h-auto"
       />
--- New/vite.config.ts
+++ New/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MeetRoomBooking/New/',
+  base: '/MeetRoomBooking/',
   build: {
     outDir: 'dist',
     sourcemap: true
