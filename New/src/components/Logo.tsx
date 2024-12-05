8fc295b51770455f2fa1aeb687ba3f3b4a80bb9b
yosof homaid <71443273+yosfgfx@users.noreply.github.com>
2024-12-05 23:31:03 +0300

    Update Logo.tsx

--- New/src/components/Logo.tsx
+++ New/src/components/Logo.tsx
@@ -3,8 +3,8 @@ import { FC } from 'react';
 export const Logo: FC = () => {
   return (
     <div className="fixed top-8 right-4 z-20">
-      <img 
-        src="/MeetRoomBooking/New/assets/logo-new.png"
+      <img         
+        src={require('../assets/logo-new.png')}
         alt="Logo" 
         className="max-w-[260px] w-full h-auto"
       />
