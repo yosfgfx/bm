0230eb3ab3a83794f33774ea177b479cfd47e5ac
yosof homaid <71443273+yosfgfx@users.noreply.github.com>
2024-12-05 23:42:47 +0300

    update links url

--- New/src/components/Logo.tsx
+++ New/src/components/Logo.tsx
@@ -4,7 +4,8 @@ export const Logo: FC = () => {
   return (
     <div className="fixed top-8 right-4 z-20">
       <img         
-        src="../assets/logo-new.png"
+        url="https://raw.githubusercontent.com/yosfgfx/MeetRoomBooking/main/New/assets/logo-new.png" 
+        type = "image/png"
         alt="Logo" 
         className="max-w-[260px] w-full h-auto"
       />
