b6b55816acebd16efe365cd5d475a0e22acd7445
yosof homaid <71443273+yosfgfx@users.noreply.github.com>
2024-12-05 23:21:23 +0300

    Update Logo.tsx

--- New/src/components/Logo.tsx
+++ New/src/components/Logo.tsx
@@ -4,7 +4,7 @@ export const Logo: FC = () => {
   return (
     <div className="fixed top-8 right-4 z-20">
       <img 
-        src="./main/New/assets/logo-new.png"
+        src="./New/assets/logo-new.png"
         alt="Logo" 
         className="max-w-[260px] w-full h-auto"
       />
