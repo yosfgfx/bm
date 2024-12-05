21554a0de7eae6ffdb5c2661c256c781aec80a97
yosof homaid <71443273+yosfgfx@users.noreply.github.com>
2024-12-05 22:09:39 +0300

    edit everything

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MeetRoomBooking/',
+  base: '/MeetRoomBooking/', // Replace 'MeetRoomBooking' with your repository name
   build: {
     outDir: 'dist',
     sourcemap: true
