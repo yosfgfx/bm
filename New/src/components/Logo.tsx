d191bf84abcdcc9fa58bf29fb417073b49e87156
yosof homaid <71443273+yosfgfx@users.noreply.github.com>
2024-12-06 19:59:25 +0300

    Update Logo.tsx

--- New/src/components/Logo.tsx
+++ New/src/components/Logo.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
+import { weqaalogo } from "'./assets/logo-new.png"
 
 export const Logo: FC = () => {
   return (
     <div className="fixed top-8 right-4 z-20">
       <img         
-        url="https://raw.githubusercontent.com/yosfgfx/MeetRoomBooking/main/New/assets/logo-new.png" 
-        type = "image/png"
+        src={weqaalogo}
         alt="Logo" 
         className="max-w-[260px] w-full h-auto"
       />
