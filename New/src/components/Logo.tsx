9df2b1931a96de7cb85590e40b5b23ae6895805b
yosof homaid <71443273+yosfgfx@users.noreply.github.com>
2024-12-06 20:06:35 +0300

    Update Logo.tsx

--- New/src/components/Logo.tsx
+++ New/src/components/Logo.tsx
@@ -1,12 +1,11 @@
 import { FC } from 'react';
-import { weqaalogo } from "'./assets/logo-new.png"
+import weqaalogo from "./assets/logo-new.png"
 
 export const Logo: FC = () => {
   return (
     <div className="fixed top-8 right-4 z-20">
       <img         
-        src={weqaalogo}
-        alt="Logo" 
+        src={weqaalogo} alt="Logo" 
         className="max-w-[260px] w-full h-auto"
       />
     </div>
